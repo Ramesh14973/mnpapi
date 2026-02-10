a35d684f500f50dff25727568eaff4552cc38e7e
Manisha <Manisha@ramesh001>
2026-02-10 23:09:14 +0530

    committed on 2026-02-10 for mnpapi Application

--- src/main/java/com/mnpapiv1/Mnpapiv1Application.java
+++ src/main/java/com/mnpapiv1/Mnpapiv1Application.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Mnpapiv1Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		SpringApplication.run(Mnpapiv1Application.class, args);
+		
+		
 	}
 
 }
